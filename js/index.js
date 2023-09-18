31b8f7e2602851bc674a53b62c655142e0a32905
Grácouš <84779291+Graca98@users.noreply.github.com>
2023-09-18 16:28:22 +0200

    Pokus o přidání boxu na odstraneni hvezdicek

--- js/index.js
+++ js/index.js
@@ -19,7 +19,7 @@ function copyTeckaText() {
   navigator.clipboard.writeText(readyBoxValue)
 }
 
-//! Text na jeden řádek
+//! ============================== Text na jeden řádek =============================================
 let textNaRadek = document.getElementById("textNaJednomRadku")
 let readyBoxNaRadek = document.getElementById("textNaJednomRadkuReady")
 let pocetZnaku = document.querySelector(".pocetZnaku")
@@ -33,11 +33,19 @@ textNaRadek.addEventListener("input", function handleChange(e) {
   let text = e.target.value
   let formatovanyText = text.trim().replace(/\n/g, ', ');
   formatovanyText = formatovanyText.charAt(0).toUpperCase() + formatovanyText.slice(1).toLowerCase();
-  formatovanyText = formatovanyText.replace(/:/g, ""); // nahradí dvojtečky ničím
+  formatovanyText = formatovanyText.replace(/:/g, ""); // nahradí dvojtečky za ""
   formatovanyText = formatovanyText.replace(/ ,/g, ","); // nahradí mezeru před čárkou pouze čárkou
   formatovanyText = formatovanyText.replace(/ w/g, " W"); // vezme " w" (mezera w) a nahradí " W"
   formatovanyText = formatovanyText.replace(/ db/g, " dB"); // vezme " db" (mezera db) a nahradí " dB"
-  formatovanyText = formatovanyText.replace(/ hepa/g, " HEPA"); // vezme " db" (mezera db) a nahradí " dB"
+  formatovanyText = formatovanyText.replace(/ hepa/g, " HEPA"); // vezme " hepa" (mezera hepa) a nahradí " HEPA"
+  formatovanyText = formatovanyText.replace(/ v nimh/g, " V NiMH"); // vezme " v nimh" (mezera v nimh) a nahradí " V NiMH"
+  formatovanyText = formatovanyText.replace(/ li-ion/g, " Li-Ion"); // vezme " li-ion" (mezera li-ion) a nahradí " Li-Ion"
+  formatovanyText = formatovanyText.replace(/ bpa/g, " BPA"); // vezme " bpa" (mezera ) a nahradí bpa" BPA"
+  formatovanyText = formatovanyText.replace(/ tare/g, " TARE"); // vezme " tare" (mezera tare) a nahradí " TARE"
+  formatovanyText = formatovanyText.replace(/ lcd/g, " LCD"); // vezme " lcd" (mezera lcd) a nahradí " LCD"
+
+  // formatovanyText = formatovanyText.replace(/ here/g, " here"); // vezme " " (mezera ) a nahradí " "
+
   
   // Výsledek
   readyRadekValue = readyBoxNaRadek.value = formatovanyText
@@ -49,6 +57,12 @@ textNaRadek.addEventListener("input", function handleChange(e) {
 function copyRowText() {
   navigator.clipboard.writeText(readyRadekValue)
 }
+
+
+
+//! ============================== Odstranění hvezdiček =============================================
+let hvezdickaTextareaBox = document.getElementById("")
+
 
 
 /* 
