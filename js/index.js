f3a6a9ae03baa60317b27e38eccf0c3017882f40
Grácouš <84779291+Graca98@users.noreply.github.com>
2023-09-22 12:44:29 +0200

    l

--- js/index.js
+++ js/index.js
@@ -107,6 +107,8 @@ let rozdil = document.querySelector(".rozdil");
 let text1InputSmaz = document.getElementById("textRozdilu1");
 let text2InputSmaz = document.getElementById("textRozdilu2");
 
+let differences = [];
+
 function porovnej() {
   let text1Input = document.getElementById("textRozdilu1");
   let text2Input = document.getElementById("textRozdilu2");
@@ -117,7 +119,6 @@ function porovnej() {
   let words1 = text1.split(" ");
   let words2 = text2.split(" ");
 
-  let differences = [];
   
   for (let i = 0; i < Math.min(words1.length, words2.length); i++) {
     if (words1[i] !== words2[i]) {
@@ -135,6 +136,7 @@ function porovnej() {
 function smazFourthBox() {
   text1InputSmaz.value = ""
   text2InputSmaz.value = ""
+  differences.value = ""
 }
 
 
